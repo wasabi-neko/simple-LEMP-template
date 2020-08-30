b02335d46aef33cfc3a6f6b6b0e92298ec12527e
AnJung <jimmmy0719191@gmail.com>
2020-08-30 17:02:44 +0800

    add index.php

---
+++ nginx/www/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Page</title>
+</head>
+<body>
+    <h1>A simple LEMP service</h1>
+    <?php
+        echo "an echo message form php"
+    ?>
+</body>
+</html>
